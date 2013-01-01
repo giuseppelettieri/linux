9c1f00dd04946ebf2b683cdd4b1836f8c6d577e3
Vincenzo Maffione <v.maffione@gmail.com>
2013-01-01 21:14:59 +0100

    m1000: header added

---
+++ drivers/net/ethernet/intel/m1000/m1000_emu.h
@@ -0,0 +1,61 @@
+
+/* registers index */
+enum {
+    CTRL = 0,	// control register (enable/disable rx/tx)
+    IE,		// interrupt mask
+    NTFY,	// notification register, used to notify the hypervisor
+    CSBBAL,     // communication status block base address low
+    CSBBAH,	// communication status block base address high
+
+    RXRTH,	// size threshold that specify what rx ring the hardware should use when receiving a packet
+
+    RX0RBAL,	// RX0 ring base address low
+    RX0RBAH,	// RX0 ring base address high
+    RX0RSZ,	// RX0 ring buffer size (in bytes)
+
+    RX1RBAL,	// RX1 ring base address low
+    RX1RBAH,	// RX1 ring base address high
+    RX1RLEN,	// RX1 ring length (in descriptors)
+
+    TXRBAL,
+    TXRBAH,
+    TXRLEN,
+
+    HWADDRL,
+    HWADDRH,
+};
+
+/* communication status block index */
+enum {
+    RX0HNTR = 0,// RX0 hardware next to receive
+    RX0SNTR,	// RX0 software next to receive
+
+    RX1HNTR,	// RX1 hardware next to receive
+    RX1SNTR,	// RX1 software next to receive
+    RX1SNTP,	// RX1 software next to prepare
+
+    TXHNTS,	// TX hardware next to send
+    TXSNTS,	// TX software next to send
+    TXSNTC,	// TX software netx to clean
+
+    NTFYMASK,	// the guest specifies if it wants to be notified by the HV
+};
+
+#define M1000_NTFYMASK_RX   0x00000001  // there are new rx frames in the ring
+
+
+struct m1000_descriptor {
+    uint64_t buffer_address;
+    uint32_t buffer_length;
+    uint32_t padding;
+};
+
+#define M1000_TX_ENABLED    0x00000001
+#define M1000_RX_ENABLED    0x00000002
+
+#define M1000_NTFY_TXD	    0x00000001  /* there are tx descriptors ready to be processed by the board */
+#define M1000_NTFY_RXD	    0x00000002  /* there are rx descriptors to be returned to the board */
+#define M1000_NTFY_IC	    0X00000004  /* interrupt clear */
+
+/* this is the size past which hardware will drop packets when setting LPE=0 */
+#define MAXIMUM_ETHERNET_VLAN_SIZE 1522
