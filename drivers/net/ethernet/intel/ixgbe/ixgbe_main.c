rambodrahmani/linux-pspat

ixgbe: move tx ring init to support tx head wb

drivers/net/ixgbe/ixgbe_main.c
@@ -2796,12 +2796,12 @@ static int ixgbe_up_complete(struct ixgbe_adapter *adapter)
 	for (i = 0; i < adapter->num_tx_queues; i++) {
 		j = adapter->tx_ring[i].reg_idx;
 		txdctl = IXGBE_READ_REG(hw, IXGBE_TXDCTL(j));
+#ifdef DEV_NETMAP // XXX i and j are the same ?
+		txdctl = ixgbe_netmap_configure_tx_ring(adapter, j, txdctl);
+#endif /* DEV_NETMAP */
 		txdctl |= IXGBE_TXDCTL_ENABLE;
 		IXGBE_WRITE_REG(hw, IXGBE_TXDCTL(j), txdctl);
 
-#ifdef DEV_NETMAP // XXX i and j are the same ?
-		ixgbe_netmap_configure_tx_ring(adapter, j);
-#endif /* DEV_NETMAP */
 
 	}
 

giuseppelettieri/linux

drivers/net/ethernet/intel/ixgbe/ixgbe_main.c
@@ -3577,6 +3577,10 @@ void ixgbe_configure_tx_ring(struct ixgbe_adapter *adapter,
 	memset(ring->tx_buffer_info, 0,
 	       sizeof(struct ixgbe_tx_buffer) * ring->count);
 
+#ifdef DEV_NETMAP
+	txdctl = ixgbe_netmap_configure_tx_ring(adapter, reg_idx, txdctl);
+#endif /* DEV_NETMAP */
+
 	/* enable queue */
 	IXGBE_WRITE_REG(hw, IXGBE_TXDCTL(reg_idx), txdctl);
 
@@ -3591,10 +3595,7 @@ void ixgbe_configure_tx_ring(struct ixgbe_adapter *adapter,
 		txdctl = IXGBE_READ_REG(hw, IXGBE_TXDCTL(reg_idx));
 	} while (--wait_loop && !(txdctl & IXGBE_TXDCTL_ENABLE));
 	if (!wait_loop)
-		hw_dbg(hw, "Could not enable Tx Queue %d\n", reg_idx);
-#ifdef DEV_NETMAP
-	ixgbe_netmap_configure_tx_ring(adapter, reg_idx);
-#endif /* DEV_NETMAP */
+		e_err(drv, "Could not enable Tx Queue %d\n", reg_idx);
 }
 
 static void ixgbe_setup_mtqc(struct ixgbe_adapter *adapter)