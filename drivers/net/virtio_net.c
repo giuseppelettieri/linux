14abfb961e5cd3f260cb2408629600a9cba45b49
Giuseppe Lettieri <giuseppe.lettieri@unipi.it>
2020-12-17 18:56:01 +0100

    virtio: play well with xdp and napi_tx

--- drivers/net/virtio_net.c
+++ drivers/net/virtio_net.c
@@ -784,15 +784,12 @@ static int virtnet_open(struct net_device *dev)
 	int i;
 #ifdef DEV_NETMAP
         int ok = virtio_netmap_init_buffers(vi);
-
-        if (ok) {
-            for (i = 0; i < vi->max_queue_pairs; i++)
-		virtnet_napi_enable(&vi->rq[i]);
-            return 0;
-        }
 #endif
 
 	for (i = 0; i < vi->max_queue_pairs; i++) {
+#ifdef DEV_NETMAP
+		if (!ok)
+#endif
 		if (i < vi->curr_queue_pairs)
 			/* Make sure we have some buffers: if oom use wq. */
 			if (!try_fill_recv(&vi->rq[i], GFP_KERNEL))
