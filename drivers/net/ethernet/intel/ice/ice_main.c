1bf51b3a03fad721413cc3f5a68e1965e1511027
Michio Honda <michio.honda@ed.ac.uk>
2023-04-21 12:40:46 +0100

    netmap: ice patch

--- drivers/net/ethernet/intel/ice/ice_main.c
+++ drivers/net/ethernet/intel/ice/ice_main.c
@@ -43,6 +43,11 @@ MODULE_PARM_DESC(debug, "netif level (0=none,...,16=all)");
 
 static DEFINE_IDA(ice_aux_ida);
 
+#if defined(CONFIG_NETMAP) || defined(CONFIG_NETMAP_MODULE)
+#define NETMAP_ICE_LIB
+#include <ice_netmap_linux.h>
+#endif
+
 static struct workqueue_struct *ice_wq;
 static const struct net_device_ops ice_netdev_safe_mode_ops;
 static const struct net_device_ops ice_netdev_ops;
@@ -4497,6 +4502,9 @@ probe_done:
 		dev_warn(dev, "RDMA is not supported on this device\n");
 	}
 
+#ifdef DEV_NETMAP
+	ice_netmap_attach(pf);
+#endif
 	return 0;
 
 err_init_aux_unroll:
@@ -4600,6 +4608,9 @@ static void ice_remove(struct pci_dev *pdev)
 	if (!pf)
 		return;
 
+#ifdef DEV_NETMAP
+	ice_netmap_detach(pf);
+#endif /* DEV_NETMAP */
 	for (i = 0; i < ICE_MAX_RESET_WAIT; i++) {
 		if (!ice_is_reset_in_progress(pf->state))
 			break;
